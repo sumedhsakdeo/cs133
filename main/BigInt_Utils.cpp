a130edd897e06128ff88cbb62f20dc1e22e9c9c8
Sumedh Vivek Sakdeo <ssakdeo@cs.ucla.edu>
2011-05-18 23:55:42 -0700

    utils

---
+++ main/BigInt_Utils.cpp
@@ -0,0 +1,8 @@
+#include	"BigInt_Utils.h"
+
+using namespace std;
+
+vector<int> BigInt_Utils :: string_to_vector (const string &str)	{
+	vector to_return(0);
+	return to_return;
+}
---
+++ main/BigInt_Utils.h
@@ -0,0 +1,13 @@
+#ifndef _BIG_INT_UTILS_H_
+#define _BIG_INT_UTILS_H_
+
+#include <vector>
+#include <string>
+
+class BigInt_Utils	{
+	public:
+		static std::vector<int> string_to_vector(const std::string &);
+};
+
+#endif //_BIG_INT_UTILS_H_
+
