64436527836c2cfa7dc1b9897d9f42e1dc1d02ba
spradhan <spradhan@cs.ucla.edu>
2011-06-02 22:16:47 -0700

    serial division

---
+++ main/test/testgens/testgenand.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import random
+import math
+
+cases = 200
+print cases
+
+doAnd = True
+doOr = False
+doExor = False
+
+for j in range(cases):
+    a = 0
+    for i in range(random.randint(50,500)):
+        digit = random.randint(0, 4294967295)
+        a = a + digit * pow(4294967296,i)
+    print a
+
+    b = 0
+    for i in range(random.randint(50,500)):
+        digit = random.randint(0, 4294967295)
+        b = b + digit * pow(4294967296,i)
+    print b
+    
+    if doAnd:
+        print a & b
+    if doOr:
+        print a | b
+    if doExor:
+        print a ^ b
+
---
+++ main/test/testgens/testgenequal.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import random
+import math
+
+#sum = 0
+for j in range(100):
+    eqlflag = random.randint(0,1)
+
+    a = 0
+    for i in range(random.randint(50,500)):
+        digit = random.randint(0, 4294967295)
+        a = a + digit * pow(4294967296,i)
+        #print i, digit
+    #sum = sum + a
+    print a
+
+    b = 0
+    for i in range(random.randint(50,500)):
+        digit = random.randint(0, 4294967295)
+        b = b + digit * pow(4294967296,i)
+        #print i, digit
+    #sum = sum + a
+    if eqlflag == 1:
+        b = a
+    print b
+
+    if a == b:
+        print '1'
+    else:
+        print '0'
+
