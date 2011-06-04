ec5a11e29d5f1e41bd5e1180d1c42f7514b9d19b
Sumedh Vivek Sakdeo <ssakdeo@cs.ucla.edu>
2011-06-03 23:10:45 -0700

    default data size 4000 digits

--- main/run-test.sh
+++ main/run-test.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 make clean
-make run-test DATA_SIZE=10
-make clean
-make run-test DATA_SIZE=100
-make clean
-make run-test DATA_SIZE=1000
-make clean
-make run-test DATA_SIZE=10000
+make run-test DATA_SIZE=4000
--- main/test/p_add.cpp
+++ main/test/p_add.cpp
@@ -12,98 +12,12 @@ using namespace std;
 
 int main(int argc, char **argv)  {
 
- /*   ifstream fin(argv[1], ifstream::in);
+	BigUInt op1(argv[1]), op2(argv[2]);
 
-    int i = 0;
-    BigUInt zero("0");
-    BigUInt sum("0");
-    while (i < 100)
-    {
-        string op;
-        fin>>op;
-        BigUInt b(op);
-        sum = sum + b;
-        i++;
-    }
-    string result;
-    fin >> result;
-    if (!result.compare(sum.ToString())) {
-        cout << "sum sucess" << endl;
-    } else { 
-        cerr << "sum failure" << endl;
-    }
+	cout << " Sum: "<< (op1+op2).ToString() << endl;
+	cout << " Sub: "<< (op1-op2).ToString() << endl;
+	cout << " Mult: "<< (op1*op2).ToString() << endl;
+	cout << " Div: "<< (op1/op2).ToString() << endl;
 
-    BigUInt sub;
-    sub = sum - sum;
-    if (!sub.ToString().compare(zero.ToString())) {
-        cout << "SUB Success" << endl;
-    } else {
-        cerr << "SUB Failure" << endl;
-    }
-
-    BigUInt mul, op1("123451"), op2("12345");
-    mul = op1 * op2;
-    cout << mul.ToString();
-    if (!mul.ToString().compare(zero.ToString())) {
-        cout << "MUL Success" << endl;
-    } else {
-        cerr << "MUL Failure" << endl;
-    }
-
-    BigUInt anded;
-    anded = zero & sum;
-    if (!anded.ToString().compare(zero.ToString())) {
-        cout << "And Success" << endl;
-    } else {
-        cerr << "And Failure" << endl;
-    }
-    
-    BigUInt ored;
-    ored = zero | sum;
-    if (!ored.ToString().compare(sum.ToString())) {
-        cout << "OR Success" << endl;
-    } else {
-        cerr << "OR Failure" << endl;
-    }
-
-    BigUInt xored;
-    xored = zero ^ sum;
-    if (!xored.ToString().compare(sum.ToString())) {
-        cout << "XOR Success" << endl;
-    } else {
-        cerr << "XOR Failure" << endl;
-    }
-    
-    BigUInt noted("18446744073709551615");
-    noted = ~noted;
-    if (!noted.ToString().compare(zero.ToString())) {
-        cout << "NOT Success" << endl;
-    } else {
-        cerr << "NOT Failure" << endl;
-    }
-
-*/
-    
-    BigUInt op11("123456789123456789"),op21("987654321123456789123456789");
-
-    cout << (op21 / op11).ToString();
-
-
- /*   int count = 1000;
-    while(count--)  {
-        vector<int> v1(100);
-        vector<int> v2(100);
-
-        for (int i=0; i<100; i++) {
-            v1[i] = i;
-            v2[i] = i;
-        }
-
-       vector<int> v3 = Parallelizer::executeBatchRequest<int,int>(v1, v2, ADD,
-       v1.size(), );  
-       for (int i=0; i < v3.size(); i++)
-            cout << v3[i] << endl;
-    }
-   */ 
     return 0;
 }
